4cee17716e8bb7ecdb599aacef50c280a92fbc4b
Mohsin Ghani <Mohsinghani.777@gmail.com>
2023-03-17 15:51:52 +0500

    first commit

---
+++ public/assets/images/index.js
@@ -0,0 +1,30 @@
+import milto from "./milto.png";
+import dragon from "./dragon.png";
+import Roblox from "./Roblox.png";
+import Vector from "./Vector.png";
+import logo from "./logo.png";
+import twiter from "./twiter.png";
+import coin from "./coin.png";
+import union from "./union.png";
+import subtract from "./subtract.png";
+import vector3 from "./vector3.png";
+import union2 from "./union2.png";
+
+import rolboxpapa from "./rolboxPapa.png";
+import cambro from "./cambro.png";
+
+export default {
+  milto,
+  dragon,
+  Roblox,
+  Vector,
+  logo,
+  twiter,
+  coin,
+  union,
+  subtract,
+  vector3,
+  union2,
+  rolboxpapa,
+  cambro,
+};
