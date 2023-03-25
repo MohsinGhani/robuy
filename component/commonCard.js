e2016918930592e081b2ad633160a4984d35659d
mohsinghani <mohsinghani.777@gmail.com>
2023-03-25 17:40:58 +0500

    minor change

--- component/commonCard.js
+++ component/commonCard.js
@@ -6,17 +6,43 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import products from "../pages/shared/json/productCards";
-import images from "../public/assets/images/index";
+// import images from "../public/assets/images/index";
 import Image from "next/image";
+import { createClient } from "contentful";
+import { useState, useEffect } from "react";
+
 const CommonCard = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const client = createClient({
+      space: "c288e1xhsyct",
+      accessToken: "9F8haQVl_uqqdxbrDbTh6noeplOE4qbhBHNn9CekcLo",
+    });
+
+    client
+      .getEntries({ content_type: "blog" })
+      .then((response) => {
+        // Handle the retrieved entries here
+        console.log(response.items);
+        setBlogs(response.items);
+      })
+
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="productCard_container">
-      {products.products?.map((product) => {
+      {blogs?.map((blog) => {
+        console.log("🚀 ~ blog:", blog);
+
         return (
           <Card sx={{ maxWidth: 320 }}>
             <div className="imgButton">
-              <Image src={images.milto} />
-              <Button variant="contained3">Новости</Button>
+              <img src={blog?.fields?.image?.fields?.file?.url} />
+              <Button variant="contained3">{blog?.fields?.tags}</Button>
             </div>
             <CardContent className="firstCard">
               <div className="box">
@@ -24,9 +50,9 @@ const CommonCard = () => {
                 <div className="box2"></div>
                 <div className="box3"></div>
                 <div className="box4"></div>
-                <Typography variant="h5">{product.title}</Typography>
+                <Typography variant="h5">{blog?.fields?.title}</Typography>
                 <Typography variant="body2" color="text.secondary">
-                  {product.decription}
+                  {blog?.fields?.descriptions}
                 </Typography>
               </div>
             </CardContent>
